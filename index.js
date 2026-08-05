a56d14824e9a9dbc61ddd24321b5dfa0f59af101
Yannik0505 <yannikweber84@gmail.com>
2026-08-05 15:53:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -523,7 +523,7 @@ Er wird sich zeitnah um dich kümmern!
 // VOICE SUPPORT WARTERAUM
 // =======================
 
-client.on(Events.VoiceStateUpdate, async (oldState, newState) => {
+client.on(Events.VoiceStateUpdate, async (oldState, newState) => });
 
     try {
 
@@ -624,7 +624,14 @@ Bitte kümmert euch schnellstmöglich um den Spieler.
 
 
 });
-    // =======================
+
+
+              }
+
+});
+
+
+// =======================
 // LOGIN
 // =======================
 
